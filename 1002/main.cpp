06b4af017159afa913c30309c6a1428edc47461e
Luncert <lijingweisee@qq.com>
2018-09-03 18:45:12 +0800

    update

--- 1002/main.cpp
+++ 1002/main.cpp
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <time.h>
+#include <string.h>
+
+// Hash + QuickSort
 
 typedef struct _Entry {
     char key[8];
@@ -10,34 +12,44 @@ typedef struct _Entry {
 } Entry;
 
 Entry** table;
-int mapSize;
-int num;
+int mapSize = 0;
+int slotSize = 0;
+int num = 0;
 
-unsigned long ELFhash(char* key) {
+inline unsigned long hashcode(char* key) {
     unsigned long h = 0;
+	unsigned long g;
     for (int i = 0; i < 7; i++) {
         h = (h << 4) + key[i];
-        unsigned long g = h & 0xF0000000L;
+        g = h & 0xF0000000L;
         if(g) h ^= g >> 24; 
         h &= ~g; 
     }
-    return h;
+    return h ^ (h >> 16);
 }
 
-Entry* newEntry(char key[7]) {
+Entry* newEntry(char* key) {
     Entry* ep = (Entry*)malloc(sizeof(Entry));
-    for (int i = 0; i < 7; i++)
+    for (int i = 0; i < 8; i++)
         ep->key[i] = key[i];
     ep->value = 1;
+	ep->next = NULL;
     return ep;
 }
 
-int put(char key[7]) {
-    unsigned long hash = ELFhash(key);
+inline void print(char* buf, int start, int stop) {
+	for (int i = start; i < stop; i++)
+		putchar(buf[i]);
+}
+
+inline int put(char* key) {
+    unsigned long hash = hashcode(key);
     int index = hash % num;
     Entry *entry = table[index], *cur;
-    if (entry == NULL)
+    if (entry == NULL) {
         table[index] = newEntry(key);
+		slotSize++;
+	}
     else {
         do {
             if (strcmp(entry->key, key) == 0) {
@@ -50,6 +62,28 @@ int put(char key[7]) {
     }
     mapSize++;
     return index;
+}
+
+int partsort(Entry** array, int left, int right) {
+    Entry *&cen = array[right], *tmp;
+    while(left < right) {
+		while(left < right && strcmp(array[left]->key, cen->key) <= 0) ++left;
+		while(left < right && strcmp(array[right]->key, cen->key) >= 0) --right;
+		tmp = array[left];
+		array[left] = array[right];
+		array[right] = tmp;
+    }
+	tmp = array[left];
+	array[left] = cen;
+	cen = tmp;
+	return left;
+}
+
+void fastsort(Entry** array, int left, int right) {
+    if(left >= right) return;
+    int index = partsort(array, left, right);
+    fastsort(array, left, index - 1);
+    fastsort(array, index + 1, right);
 }
 
 void clean() {
@@ -67,9 +101,10 @@ void clean() {
 void readData() {
     scanf("%d\n", &num);
     table = (Entry**)malloc(num * sizeof(Entry*));
-    char key[7];
+	for (int i = 0; i < num; i++) table[i] = NULL;
+	char key[8] = {0};
     for (int i = 0; i < num; i++) {
-        // 读号码,并做预处理
+        // 读取一行
         char c;
         int n = 0;
         while ((c = getchar()) != '\n' && c > 0) {
@@ -86,48 +121,39 @@ void readData() {
 }
 
 void output() {
-    Entry* indexs[mapSize];
+    Entry** indexs = (Entry**)malloc(mapSize * sizeof(Entry*));
     int size = 0;
-    // 创建字典序索引
+    // ��ȡ����
     for (int i = 0; i < num; i++) {
         Entry *entry = table[i];
         while (entry != NULL) {
-            if (entry->value > 1) {
-                // 排序
-                indexs[size] = entry;
-                for (int j = 0; j < size; j++) {
-                    if (strcmp(indexs[j]->key, entry->key) > 0) {
-                        for (int k = size; k > j; k--)
-                            indexs[k] = indexs[k - 1];
-                        indexs[j] = entry;
-                        break;
-                    }
-                }
-                size++;
-            }
+			if (entry->value > 1)
+				indexs[size++] = entry;
             entry = entry->next;
         }
     }
+	// 快排
+	fastsort(indexs, 0, size - 1);
     // 输出
     if (size > 0) {
         for (int i = 0; i < size; i++) {
-            for (int j = 0; j < 7; j++) {
-                if (j == 3) putchar('-');
-                putchar(indexs[i]->key[j]);
-            }
+			print(indexs[i]->key, 0, 3);
+			putchar('-');
+			print(indexs[i]->key, 3, 7);
             printf(" %d\n", indexs[i]->value);
         }
     }
     else printf("No duplicates.\n");
-    printf("%d\n", size);
+	// printf("mapSize: %d slotSize: %d mapMemSize: %d\n", mapSize, slotSize, num);
+    // printf("time: %f ms\n", (double)clock() / CLOCKS_PER_SEC * 1000);
+	free(indexs);
     clean();
 }
 
 int main() {
-    freopen("test/input.txt", "rb", stdin);
-    freopen("test/output-result.txt", "w", stdout);
+    //freopen("input.txt", "rb", stdin);
+    //freopen("output.txt", "w", stdout);
     readData();
     output();
-    printf("time: %f ms\n", (double)clock() / CLOCKS_PER_SEC * 1000);
     return 0;
 }
